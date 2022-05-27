f7ccd44df2da3149b93d03c251731de32ea37c00
Lucie-Baborova <lucie.babor@gmail.com>
2022-05-27 13:39:10 +0200

    my favorites view updated

--- client/src/views/MyFavoritesView.js
+++ client/src/views/MyFavoritesView.js
@@ -3,21 +3,22 @@ import "./MyFavoritesView.css";
 import { Link } from 'react-router-dom';
 
 function MyFavoritesView(props) {
+    //console.log(props.myFavorites)
     return (
         <div className="MyFavoritesView">
                       {
                 props.myFavorites.map(f => (
-                  <figure key = {f.id} >
-                    <Link to={'/recipes/'+f.id}>
-                      <img key={f.id}
-                            id={f.id}
-                            src={f.image}
+                  <figure key = {f.recipe_id} >
+                    <Link to={'/recipes/'+f.recipe_id}>
+                      <img key={f.recipe_id}
+                            id={f.recipe_id}
+                            src={f.recipe_img}
                             alt="image of the meal"
-                            onClick={(e) => props.getRecipeInfoCb(f.id)}/>  
+                            onClick={(e) => props.getRecipeInfoCb(f.recipe_id)}/>  
                     </Link>
                     <figcaption>
-                        {f.title}
-                        <button onClick={(e) => props.deleteFromFavoritesCb(f.id)} title="delete" type="button">Delete</button> 
+                        {f.recipe_title}
+                        <button onClick={(e) => props.deleteFromFavoritesCb(f.recipe_id)} title="delete" type="button">Delete</button> 
                     </figcaption> 
                 </figure>
                 
