86d48508ab87d3703f77d31d8e23700ad2d10d37
Lucie-Baborova <lucie.babor@gmail.com>
2022-07-29 22:59:59 +0200

    text centered

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -7,11 +7,12 @@ function HomeView() {
     return (
       <div className="HomeView">
         <h1>WHAT'S IN MY FRIDGE?</h1>
-        <h4>Click on the fridge to insert ingredients and get your meal!</h4>
+        <h4 className="text-center">Click on the fridge to insert ingredients and get your meal!</h4>
         <div className="row text-center">
           <div className="col-offset-5">
             <Link to={'/getmeal'}>
-            <img src={fridgeImg} alt="Big Fridge Vector - Refrigerator@seekpng.com" />
+            <img src={fridgeImg} alt="Big Fridge Vector - Refrigerator@seekpng.com" /> 
+            {/* img source seekpng: https://www.seekpng.com/ipng/u2e6w7y3i1e6t4r5_big-fridge-vector-refrigerator/ */}
             </Link>
           </div>
         </div>
